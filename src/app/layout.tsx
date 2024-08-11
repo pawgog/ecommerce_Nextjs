b27f00646e194f16b1ab8e4da5d1730946095812
pawgog <pgogola10@gmail.com>
2024-08-11 14:56:44 +0200

    Create home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Icons } from "@/components/Icons";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="relative min-h-screen isolate overflow-hidden border-b border-gray-200 bg-white">
+          <div className="mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-32 lg:flex gap-16 lg:px-8 lg:py-24">
+            <div className="w-full h-full flex flex-col items-center gap-4">
+              <Icons.Sparkle className="w-16 h-16" />
+              <h1 className="tracking-tight text-4xl sm:text-6xl font-bold">
+                EcommerceShop
+              </h1>
+            </div>
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
