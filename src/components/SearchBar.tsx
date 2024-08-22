b14317f6782ccbac3308e510dee9ce4d6b1e9857
pawgog <pgogola10@gmail.com>
2024-08-22 21:03:01 +0200

    Add search icon and reference to input

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,12 +1,29 @@
+"use client";
+
+import { KeyboardEvent, useRef } from "react";
+import { Search } from "lucide-react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 
 const SearchBar = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const handleKeyUp = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Escape") {
+      inputRef?.current?.blur();
+    }
+  };
+
   return (
     <div className="relative w-full h-14 flex flex-col bg-white">
       <div className="relative h-14 z-10 rounded-md">
-        <Input className="absolute inset-0 h-full" />
-        <Button className="absolute right-0 inset-y-0 h-full rounded-l-none" />
+        <Input
+          ref={inputRef}
+          onKeyUp={(e) => handleKeyUp(e)}
+          className="absolute inset-0 h-full"
+        />
+        <Button className="absolute right-0 inset-y-0 h-full rounded-l-none">
+          <Search className="w-6 h-6" />
+        </Button>
       </div>
     </div>
   );
