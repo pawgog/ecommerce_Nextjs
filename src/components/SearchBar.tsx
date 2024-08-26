1ff856d5e0e692c83cdf5a0e5ec4c4f2643e6fdd
pawgog <pgogola10@gmail.com>
2024-08-26 22:09:32 +0200

    Init query to search functionality

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,16 +1,27 @@
 "use client";
 
-import { KeyboardEvent, useRef } from "react";
+import { KeyboardEvent, useRef, useState, useTransition } from "react";
+import { useRouter } from "next/navigation";
 import { Search } from "lucide-react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 
 const SearchBar = () => {
+  const [query, setQuery] = useState();
+  const [isSearching, setTransition] = useTransition();
+  const router = useRouter();
   const inputRef = useRef<HTMLInputElement>(null);
+
   const handleKeyUp = (e: KeyboardEvent<HTMLInputElement>) => {
     if (e.key === "Escape") {
       inputRef?.current?.blur();
     }
+  };
+
+  const search = () => {
+    setTransition(() => {
+      router.push(`/search?query=${query}`);
+    });
   };
 
   return (
