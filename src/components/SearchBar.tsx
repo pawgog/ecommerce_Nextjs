0d86dc81b22b4447304719558e2d800200bf35b8
pawgog <pgogola10@gmail.com>
2024-08-27 19:17:29 +0200

    Add missing functionality to query function

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,21 +2,15 @@
 
 import { KeyboardEvent, useRef, useState, useTransition } from "react";
 import { useRouter } from "next/navigation";
-import { Search } from "lucide-react";
+import { Loader, Search } from "lucide-react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 
 const SearchBar = () => {
-  const [query, setQuery] = useState();
+  const [query, setQuery] = useState("");
   const [isSearching, setTransition] = useTransition();
   const router = useRouter();
   const inputRef = useRef<HTMLInputElement>(null);
-
-  const handleKeyUp = (e: KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "Escape") {
-      inputRef?.current?.blur();
-    }
-  };
 
   const search = () => {
     setTransition(() => {
@@ -24,16 +18,36 @@ const SearchBar = () => {
     });
   };
 
+  const handleKeyUp = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      search();
+    }
+    if (e.key === "Escape") {
+      inputRef?.current?.blur();
+    }
+  };
+
   return (
     <div className="relative w-full h-14 flex flex-col bg-white">
       <div className="relative h-14 z-10 rounded-md">
         <Input
+          value={query}
           ref={inputRef}
+          disabled={isSearching}
+          onChange={(e) => setQuery(e.target.value)}
           onKeyUp={(e) => handleKeyUp(e)}
           className="absolute inset-0 h-full"
         />
-        <Button className="absolute right-0 inset-y-0 h-full rounded-l-none">
-          <Search className="w-6 h-6" />
+        <Button
+          onClick={search}
+          disabled={isSearching}
+          className="absolute right-0 inset-y-0 h-full rounded-l-none"
+        >
+          {isSearching ? (
+            <Loader className="w-6 h-6 animate-spin" />
+          ) : (
+            <Search className="w-6 h-6" />
+          )}
         </Button>
       </div>
     </div>
