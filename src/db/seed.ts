0e4f2c8637b3b601bdbf6bc4c6d32b344faf4529
pawgog <pgogola10@gmail.com>
2024-09-17 18:21:22 +0200

    Implemented vectorize logic

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,14 +1,14 @@
 import { faker } from '@faker-js/faker'
 import { neon } from '@neondatabase/serverless'
-// import { Index } from '@upstash/vector'
+import { Index } from '@upstash/vector'
 import * as dotenv from 'dotenv'
 import { drizzle } from 'drizzle-orm/neon-http'
-// import { vectorize } from '../lib/vectorize'
+import { vectorize } from '../lib/vectorize'
 import { productTable } from './schema'
 
 dotenv.config()
 
-// const index = new Index()
+const index = new Index()
 
 async function main() {
   const connector = neon(process.env.DB_URL!)
@@ -68,17 +68,17 @@ async function main() {
   products.forEach(async (product) => {
     await db.insert(productTable).values(product).onConflictDoNothing()
 
-    // await index.upsert({
-    //   id: product.id!,
-    //   vector: await vectorize(`${product.name}: ${product.description}`),
-    //   metadata: {
-    //     id: product.id,
-    //     name: product.name,
-    //     description: product.description,
-    //     price: product.price,
-    //     imageId: product.imgId,
-    //   },
-    // })
+    await index.upsert({
+      id: product.id!,
+      vector: await vectorize(`${product.name}: ${product.description}`),
+      metadata: {
+        id: product.id,
+        name: product.name,
+        description: product.description,
+        price: product.price,
+        imageId: product.imgId,
+      },
+    })
   })
 }
 
