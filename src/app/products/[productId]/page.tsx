6be498c3df0d6ebcf5caac343f7f8d316a6047f4
pawgog <pgogola10@gmail.com>
2024-10-08 18:43:37 +0200

    Implement product details information

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -26,6 +26,26 @@ const Product = async ({ params }: ProductProps) => {
     <div className="py-8 pb-8 px-12 divide-y divide-zinc-100 bg-white shadow-md rounded-b-md">
       <div>
         <BackButton />
+
+        <div className="mt-4">
+          <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            {product.name}
+          </h1>
+        </div>
+
+        <div className="mt-4">
+          <div className="flex items-center">
+            <p className="font-medium text-gray-900">
+              ${product.price.toFixed(2)}
+            </p>
+          </div>
+
+          <div className="mt-4 space-y-6">
+            <p className="text-base max-w-prose text-muted-foreground">
+              {product.description}
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
