4f367b96dd3f55216b5ea4fe2ad87edf741220df
pawgog <pgogola10@gmail.com>
2024-11-03 14:55:02 +0100

    Move all actions to one file

--- src/actions/productActions.ts
+++ src/actions/productActions.ts
@@ -1,14 +1,34 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
+import { eq, sql } from "drizzle-orm";
 
 import { db } from "@/db";
-import { Product, productsCart } from "@/db/schema";
+import { Product, productsCart, productTable } from "@/db/schema";
+
+export const getAllProducts = async () => {
+  return await db.select().from(productTable);
+}
+
+export const getSingleProduct = async (productId: string) => {
+  return await db.select().from(productTable).where(eq(productTable.id, productId));
+}
 
 export const getProductsCart = async () => {
-  const data = await db.select().from(productsCart);
-  return data;
+  return await db.select().from(productsCart);
 };
+
+export const filterAllProducts = async (query: string) => {
+  return await db.select().from(productTable)
+  .where(
+    sql`to_tsvector('simple', lower(${productTable.name} || ' ' || ${
+      productTable.description
+    })) @@ to_tsquery('simple', lower(${query
+      .trim()
+      .split(" ")
+      .join(" & ")}))`
+  ).limit(5);
+}
 
 export const addProductToCart = async (product: Product) => {
   await db.insert(productsCart).values(product);
